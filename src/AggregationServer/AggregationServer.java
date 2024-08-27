3dae8ca16e09f8b69c466aa471cc5856e1b7f8d3
Zachary d'Assumpcao <a1850852@student.adelaide.edu.au>
2024-08-27 14:52:02 +0930

    Modified Aggregation Server to be able to continuously handle multiple clients at the same time.

--- src/AggregationServer/AggregationServer.java
+++ src/AggregationServer/AggregationServer.java
@@ -5,32 +5,58 @@ import java.io.*;
 
 public class AggregationServer {
   private ServerSocket server_socket;
-  private Socket client_socket;
-  private PrintWriter out;
-  private BufferedReader in;
 
-  public void start(int port) throws IOException {
-    server_socket = new ServerSocket(port);
-    client_socket = server_socket.accept();
-    out = new PrintWriter(client_socket.getOutputStream(), true);
-    in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-    String greeting = in.readLine();
-    if ("hello server".equals(greeting)) {
-      out.println("hello client");
-    } else {
-      out.println("unrecognised greeting");
+  public static void main(String[] args) {
+    AggregationServer server = new AggregationServer();
+    try {
+      server.start(6666);
+    } catch (IOException e) {
+      System.err.println("Problem starting server");
+      e.printStackTrace();
     }
   }
 
+  public void start(int port) throws IOException {
+    server_socket = new ServerSocket(port);
+    while (true)
+      new ClientHandler(server_socket.accept()).start();
+  }
+
   public void stop() throws IOException {
-    in.close();
-    out.close();
-    client_socket.close();
     server_socket.close();
   }
 
-  public static void main(String[] args) throws IOException {
-    AggregationServer server = new AggregationServer();
-    server.start(6666);
+  private static class ClientHandler extends Thread {
+    private Socket client_socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket) {
+      this.client_socket = socket;
+    }
+
+    public void run() {
+      try {
+        out = new PrintWriter(client_socket.getOutputStream(), true);
+        in = new BufferedReader(
+            new InputStreamReader(client_socket.getInputStream()));
+
+        String input_line;
+        while ((input_line = in.readLine()) != null) {
+          if (".".equals(input_line)) {
+            out.println("bye");
+            break;
+          }
+          out.println(input_line);
+        }
+
+        in.close();
+        out.close();
+        client_socket.close();
+      } catch (IOException e) {
+        System.err.println("Server Client Handler Error");
+        e.printStackTrace();
+      }
+    }
   }
 }
--- src/Test/ClientTest.java
+++ src/Test/ClientTest.java
@@ -11,10 +11,28 @@ import Client.Client;
 
 public class ClientTest {
   @Test
-  public void SimpleMessageSend() throws UnknownHostException, IOException {
-    Client client = new Client();
-    client.startConnection("127.0.0.1", 6666);
-    String response = client.sendMessage("hello server");
-    assertEquals("hello client", response);
+  public void SimpleServerEcho1() throws UnknownHostException, IOException {
+    Client client1 = new Client();
+    client1.startConnection("127.0.0.1", 6666);
+    String msg1 = client1.sendMessage("hello");
+    String msg2 = client1.sendMessage("world");
+    String terminate = client1.sendMessage(".");
+
+    assertEquals(msg1, "hello");
+    assertEquals(msg2, "world");
+    assertEquals(terminate, "bye");
+  }
+
+  @Test
+  public void SimpleServerEcho2() throws UnknownHostException, IOException {
+    Client client2 = new Client();
+    client2.startConnection("127.0.0.1", 6666);
+    String msg1 = client2.sendMessage("hello");
+    String msg2 = client2.sendMessage("world");
+    String terminate = client2.sendMessage(".");
+
+    assertEquals(msg1, "hello");
+    assertEquals(msg2, "world");
+    assertEquals(terminate, "bye");
   }
 }
